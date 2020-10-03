d6a7ba54ebb571821a887075d95d80a58fd7163a
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-03 23:35:04 +0100

    Set up initial Player reducer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import playerReducer from './reducers/playerReducer'
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(playerReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/playerReducer.js
@@ -0,0 +1,4 @@
+export default function playerReducer(state = {players: []}, action) {
+
+  return state
+}
