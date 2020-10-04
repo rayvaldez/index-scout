3655a33c33b5bcfed161fa8f259aaa6ba57dce1a
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-04 17:43:21 +0100

    Add PlayersContainer to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import PlayersContainer from './containers/playersContainer'
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        App
+        <PlayersContainer/>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
---
+++ src/containers/playersContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class PlayersContainer extends React.Component {
+
+  render() {
+    return(
+      <div>
+        PlayersContainer
+      </div>
+    )
+  }
+}
+
+export default PlayersContainer
