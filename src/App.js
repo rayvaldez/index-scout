87845a325dd517a8bb74b5230fdb8f1f43755292
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-08 17:55:58 +0100

    Add Logo and Home link to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import {Route, Link} from 'react-router-dom'
 import PlayersContainer from './containers/playersContainer'
+import logo from './images/logo.png';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
+        <img src={logo} alt="Index Scout" width="100" height="90"/>
+        <Link to="/players">Home</Link>
         <PlayersContainer/>
       </div>
     );
