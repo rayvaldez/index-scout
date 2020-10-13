36858520c94ac55289b44944416173a8200084cd
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-13 22:51:33 +0100

    Add Navigation Bar

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
+function Navigation() {
+  return (
+    <div>
+      <Navbar expand="md">
+        <img src={`images/logo.png`} alt="Index Scout" width="100" height="90"/>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/players">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/players">Players</NavLink>
+          </NavItem>
+        </Nav>
+        <NavbarText>Scout your next player...</NavbarText>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navigation;
